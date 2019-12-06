d53f60100062a43aa8a294200aaa7e90b990803d
Tanner York <tannerwyork@gmail.com>
2019-12-05 16:59:17 -0800

    [add] unity depth mapping sim

---
+++ depth-mapping-sim/Assets/calc_real_dist.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class calc_real_dist : MonoBehaviour
+{
+    public GameObject cube;
+    public GameObject cameraRight;
+    public GameObject cameraLeft;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 midpoint = (cameraLeft.transform.position + cameraRight.transform.position)/2;
+        float dist = Vector3.Distance(cube.transform.position, midpoint);
+        Debug.Log(dist);
+    }
+}
